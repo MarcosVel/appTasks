fa5d70b2a2424b805223fb6b75a91ba23e2eaaf1
MarcosVel <marcos.veloso@sempreceub.com>
2021-09-19 20:40:17 -0300

    login view finished

---
+++ src/components/Login/index.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import { Keyboard, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
+
+export default function Login() {
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  function handleLogin() {
+    alert('teste')
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() }>
+      <View style={ styles.container }>
+        <TextInput
+          placeholder='Seu e-mail'
+          style={ styles.input }
+          value={ email }
+          onChangeText={ (email) => setEmail(email) }
+        />
+
+        <TextInput
+          placeholder='******'
+          style={ styles.input }
+          value={ password }
+          onChangeText={ (senha) => setPassword(senha) }
+        />
+
+        <TouchableOpacity
+          style={ styles.btnLogin }
+          onPress={ handleLogin }
+        >
+          <Text style={ styles.loginText }>Acessar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text>Criar uma conta</Text>
+        </TouchableOpacity>
+      </View>
+    </TouchableWithoutFeedback>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    marginBottom: 10,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    height: 45,
+    width: '90%',
+    padding: 10,
+    elevation: 1,
+    fontSize: 17
+  },
+  btnLogin: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#141414',
+    height: 45,
+    width: '90%',
+    borderRadius: 8,
+    marginBottom: 10
+  },
+  loginText: {
+    fontSize: 17,
+    color: '#fff',
+  }
+})
