88bb5a0ab87ce9f8f2d8db7227e2e36ae76a5d23
MarcosVel <marcos.veloso@sempreceub.com>
2021-10-15 01:08:27 -0300

    refactoring code, tasks: App > TaskView

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { StyleSheet, Text, View, StatusBar, SafeAreaView, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, SafeAreaView, TextInput, TouchableOpacity, Keyboard, FlatList } from 'react-native';
 import Login from './src/components/Login';
-import { FontAwesome, Ionicons } from '@expo/vector-icons';
-import { FlatList } from 'react-native-gesture-handler';
-import TaskList from './src/components/TaskList';
-
+// import { FontAwesome, Ionicons } from '@expo/vector-icons';
+// import TaskList from './src/components/TaskList';
 import firebase from './src/services/firebaseConnection';
+import TaskView from './src/pages/TaskView';
 
 export default function App() {
   const [ user, setUser ] = useState(null);
@@ -120,55 +119,9 @@ export default function App() {
         !user ?
           // Create/Login view
           <Login changeStatus={ (user) => setUser(user) } />
-
           :
-
           // Task View
-          <>
-            <View style={ styles.containerTask }>
-              <TextInput
-                style={ styles.input }
-                placeholder="Tarefa..."
-                value={ newTask }
-                onChangeText={ (text) => setNewTask(text) }
-                ref={ inputRef }
-              />
-
-              <TouchableOpacity style={ styles.buttonAdd } onPress={ handleAdd }>
-                <FontAwesome name="plus" size={ 20 } color="white" />
-              </TouchableOpacity>
-            </View>
-
-            {
-              key.length > 0 && (
-                <View style={ styles.viewWarning }>
-                  <TouchableOpacity style={ styles.btnWarningCancel } onPress={ cancelEdit }>
-                    <Ionicons name="ios-close-circle-outline" size={ 24 } color="#ff0000" style={ { marginRight: 3 } } />
-                    <Text style={ { color: '#ff0000' } }>Cancelar</Text>
-                  </TouchableOpacity>
-
-                  <View style={ { flexDirection: 'row', alignItems: 'center', } }>
-                    <Ionicons name="ios-alert-circle-outline" size={ 24 } color="#9c9c9c" style={ { marginRight: 3 } } />
-                    <Text style={ { color: '#9c9c9c' } }>Você está editando uma tarefa</Text>
-                  </View>
-                </View>
-              )
-            }
-
-            <FlatList
-              data={ tasks }
-              keyExtractor={ (item) => item.key }
-              contentContainerStyle={ { paddingBottom: 50 } }
-              showsVerticalScrollIndicator={ false }
-              renderItem={ ({ item }) => (
-                <TaskList
-                  data={ item }
-                  deleteItem={ handleDelete }
-                  editItem={ handleEdit }
-                />
-              ) }
-            />
-          </>
+          <TaskView novaTask={ newTask } inputRef={ inputRef } handleAdd={ handleAdd } key={ key } cancelEdit={ cancelEdit } tasks={ tasks } deleteItem={ handleDelete } editItem={ handleEdit } />
       }
     </SafeAreaView>
   );
@@ -179,43 +132,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#f2f6fc',
     paddingHorizontal: 10,
-  },
-
-  containerTask: {
-    flexDirection: 'row',
-    paddingTop: 20,
-  },
-  input: {
-    flex: 1, // pegar toda largura
-    marginBottom: 10,
-    backgroundColor: '#fff',
-    borderRadius: 8,
-    height: 45,
-    padding: 10,
-    paddingHorizontal: 15,
-    elevation: 3,
-    fontSize: 17
-  },
-  buttonAdd: {
-    backgroundColor: '#141414',
-    height: 45,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginLeft: 10,
-    paddingHorizontal: 15,
-    borderRadius: 8,
-  },
-  buttonText: {
-    fontSize: 30,
-    color: '#fff',
-  },
-  viewWarning: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: 10
-  },
-  btnWarningCancel: {
-    flexDirection: 'row', alignItems: 'center'
   },
 });
